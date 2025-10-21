315dfef34fdc730e486fd7a10d1000e6495b3482
AldarUlyumdzhiev <aldarulyumdzhiev@yandex.ru>
2025-10-21 18:54:50 +0300

    Stage 1: Spring Cloud

--- core/main-service/src/main/java/ru/practicum/category/dto/CategoryDto.java
+++ core/main-service/src/main/java/ru/practicum/category/dto/CategoryDto.java
@@ -1,5 +1,7 @@
 package ru.practicum.category.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -13,5 +15,7 @@ public class CategoryDto {
 
     private Long id;
 
+    @NotBlank(message = "Category name must not be blank")
+    @Size(max = 50, message = "Category name length must be <= 50")
     private String name;
 }
